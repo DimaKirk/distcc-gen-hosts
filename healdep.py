6eddae1a6446b8b4b88f08d9301576e4ee260429
Konstantin Slastnoy <sikroz@ispsystem.com>
2016-04-16 04:18:24 -0400

    Тулза для починки dep файлов

---
+++ healdep.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import sys
+import subprocess
+args = sys.argv[1:]
+#print args
+subprocess.check_call(args)
+
+i = len(args) - 1
+deplocation = None
+while (i >= 0):
+	if args[i] == '-MF':
+		deplocation = args[i+1]
+		break
+	i = i - 1
+if deplocation is None:
+	exit(0)
+objdir = '/'.join(deplocation.split('/')[:-2]) + '/.obj'
+depfile = open(deplocation)
+lines = depfile.readlines()
+depfile.close()
+if not lines[0].startswith(objdir):
+	lines[0] = objdir + lines[0]
+
+depfile = open(deplocation, 'w')
+for line in lines:
+	depfile.write(line)
+depfile.close()
+#subprocess.call(['head', deplocation])
